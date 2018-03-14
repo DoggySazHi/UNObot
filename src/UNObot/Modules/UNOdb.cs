6fea7228f9acedf941d6172066c99be4636053f1
DoggySazHi Doge <doggysazhi@gmail.com>
2018-03-14 10:10:25 -0700

    Still version 1.5B1.

--- src/UNObot/Modules/UNOdb.cs
+++ src/UNObot/Modules/UNOdb.cs
@@ -9,7 +9,7 @@ namespace UNObot.Modules
     public class UNOdb
     {
         MySqlConnection conn;
-        public void Test()
+        public void GetConnectionString()
         {
             using (StreamReader r = new StreamReader("config.json"))
             {
@@ -26,7 +26,7 @@ namespace UNObot.Modules
             {
                 Console.WriteLine("Connecting to MySQL...");
                 conn.Open();
-                //Open successful? Then working!
+                // Perform database operations
             }
             catch (Exception ex)
             {
@@ -35,77 +35,89 @@ namespace UNObot.Modules
             conn.Close();
             Console.WriteLine("Successfully connected.");
         }
-        public void GetConnectionString()
-        {
-            using (StreamReader r = new StreamReader("config.json"))
-            {
-                string json = r.ReadToEnd();
-                JObject jObject = JObject.Parse(json);
-                if (jObject["connStr"] == null)
-                {
-                    Console.WriteLine("ERROR: Database string has not been written in config.json!\nIt must contain a connStr.");
-                    return;
-                }
-                conn = new MySqlConnection((string)jObject["connStr"]);
-            }
-        }
         public void AddUser(ulong id, string usrname)
         {
             if (conn == null)
                 GetConnectionString();
-            using (MySqlCommand Cmd = new MySqlCommand())
-            {
-                Cmd.Connection = conn;
-                Cmd.CommandText = "INSERT INTO Players (userid, username, inGame) VALUES(?, ?, 1) ON DUPLICATE KEY UPDATE username = ?, inGame = 1";
-                MySqlParameter p1 = new MySqlParameter();
-                p1.Value = id;
-                Cmd.Parameters.Add(p1);
+            MySqlCommand Cmd = new MySqlCommand();
+            Cmd.Connection = conn;
+            Cmd.CommandText = "INSERT INTO Players (userid, username, inGame) VALUES(?, ?, 1) ON DUPLICATE KEY UPDATE username = ?, inGame = 1";
+            MySqlParameter p1 = new MySqlParameter();
+            p1.Value = id;
+            Cmd.Parameters.Add(p1);
 
-                MySqlParameter p2 = new MySqlParameter();
-                p2.Value = usrname;
-                Cmd.Parameters.Add(p2);
-                //for third parameter
-                MySqlParameter p3 = new MySqlParameter();
-                p3.Value = usrname;
-                Cmd.Parameters.Add(p3);
-                try
-                {
-                    conn.Open();
-                    Cmd.ExecuteNonQuery();
-                }
-                catch (MySqlException ex)
-                {
-                    Console.WriteLine($"A MySQL error has been caught, Error {ex}");
-                }
-                finally
-                {
-                    conn.Close();
-                }
+            MySqlParameter p2 = new MySqlParameter();
+            p2.Value = usrname;
+            Cmd.Parameters.Add(p2);
+            //for third parameter
+            MySqlParameter p3 = new MySqlParameter();
+            p3.Value = usrname;
+            Cmd.Parameters.Add(p3);
+            try
+            {
+                conn.Open();
+                Cmd.ExecuteNonQueryAsync();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine($"A MySQL error has been caught, Error {ex}");
+            }
+            finally
+            {
+                conn.Close();
             }
         }
         public void RemoveUser(ulong id, string usrname)
         {
             if (conn == null)
                 GetConnectionString();
-            using (MySqlCommand Cmd = new MySqlCommand())
-            {
-                Cmd.Connection = conn;
-                Cmd.CommandText = "INSERT INTO Players (userid, username, inGame) VALUES(?, ?, 0) ON DUPLICATE KEY UPDATE username = ?, inGame = 0";
-                MySqlParameter p1 = new MySqlParameter();
-                p1.Value = id;
-                Cmd.Parameters.Add(p1);
+            MySqlCommand Cmd = new MySqlCommand();
+            Cmd.Connection = conn;
+            Cmd.CommandText = "INSERT INTO Players (userid, username, inGame) VALUES(?, ?, 0) ON DUPLICATE KEY UPDATE username = ?, inGame = 0";
+            MySqlParameter p1 = new MySqlParameter();
+            p1.Value = id;
+            Cmd.Parameters.Add(p1);
 
-                MySqlParameter p2 = new MySqlParameter();
-                p2.Value = usrname;
-                Cmd.Parameters.Add(p2);
-                //for third parameter
-                MySqlParameter p3 = new MySqlParameter();
-                p3.Value = usrname;
-                Cmd.Parameters.Add(p3);
+            MySqlParameter p2 = new MySqlParameter();
+            p2.Value = usrname;
+            Cmd.Parameters.Add(p2);
+            //for third parameter
+            MySqlParameter p3 = new MySqlParameter();
+            p2.Value = usrname;
+            Cmd.Parameters.Add(p3);
+            try
+            {
+                conn.Open();
+                Cmd.ExecuteNonQueryAsync();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine($"A MySQL error has been caught, Error {ex}");
+            }
+            finally
+            {
+                conn.Close();
+            }          
+        }
+        public List<ulong> GetPlayers()
+        {
+            if (conn == null)
+                GetConnectionString();
+            MySqlCommand Cmd = new MySqlCommand();
+            Cmd.Connection = conn;
+            Cmd.CommandText = "SELECT * FROM UNObot.Players WHERE inGame = 1";
+            List<ulong> players = new List<ulong>();
+            using (MySqlDataReader dr = Cmd.ExecuteReader())
+            {
                 try
                 {
                     conn.Open();
-                    Cmd.ExecuteNonQuery();
+                    Cmd.ExecuteNonQueryAsync();
+
+                    while (dr.Read())
+                    {
+                        players.Add(dr.GetUInt64(0));
+                    }
                 }
                 catch (MySqlException ex)
                 {
@@ -115,7 +127,12 @@ namespace UNObot.Modules
                 {
                     conn.Close();
                 }
+                return players;
             }
+        }
+        public void add7cards()
+        {
+            //Yes
         }
     }
 }
