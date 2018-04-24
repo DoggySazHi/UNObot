c5557ea11f48c7850f6fd12fdeb926a2cbe0a69d
DoggySazHi <william.le.huy@gmail.com>
2018-04-24 10:17:57 -0700

    Still don't run .-.

--- src/UNObot/Modules/UNOcmds.cs
+++ src/UNObot/Modules/UNOcmds.cs
@@ -1,9 +1,16 @@
+using System;
+using System.Threading.Tasks;
 using Discord.Commands;
 
 namespace DiscordBot.Modules
 {
     public class UNOcmds : ModuleBase<SocketCommandContext>
     {
-
+        [Command("seed")]
+        public async Task Seed(string seed)
+        {
+            UNOcore.r = new Random(seed.GetHashCode());
+            await ReplyAsync("Seed has been updated. I do not guarantee 100% Wild cards.");
+        }
     }
 }
--- src/UNObot/Modules/UNOcore.cs
+++ src/UNObot/Modules/UNOcore.cs
@@ -1,5 +1,6 @@
 using System;
-
+using System.Threading.Tasks;
+using System.Timers;
 namespace DiscordBot.Modules
 {
     public class Card
@@ -75,6 +76,78 @@ namespace DiscordBot.Modules
                 }
             }
             return card;
+        }
+    }
+    public class ColorConsole
+    {
+        public static void WriteLine(string Text, string Color)
+        {
+            ConsoleColor[] colors = (ConsoleColor[]) ConsoleColor.GetValues(typeof(ConsoleColor));
+            ConsoleColor behind = Console.BackgroundColor;
+            ConsoleColor front = Console.ForegroundColor;
+            bool colorFound = false;
+            foreach(var color in colors)
+            {
+                if(color.ToString() == Color)
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = color;
+                    colorFound |= true;
+                    break;
+                }
+            }
+            Console.WriteLine(Text);
+            Console.BackgroundColor = behind;
+            Console.ForegroundColor = front;
+            #if DEBUG
+            if(!colorFound)
+                WriteLine("[WARN] Attempted to WriteLine with a color that doesn't exist!", ConsoleColor.Yellow);
+            #endif
+        }
+        public static void WriteLine(string Text, ConsoleColor color)
+        {
+            ConsoleColor behind = Console.BackgroundColor;
+            ConsoleColor front = Console.ForegroundColor;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = color;
+            Console.WriteLine(Text);
+            Console.BackgroundColor = behind;
+            Console.ForegroundColor = front;
+        }
+    }
+    public class GameTimer : Timer
+    {
+        public new bool Disposed { get; set; }
+        protected override void Dispose(bool disposing) {
+            Disposed = true;
+            base.Dispose(disposing);
+        }
+    }
+    public class AFKtimer
+    {
+        static GameTimer playTimer;
+        void ResetTimer()
+        {
+            playTimer.Stop();
+            playTimer.Start();
+        }
+        void StartTimer()
+        {
+            playTimer = (GameTimer) new Timer(90000);
+            playTimer.AutoReset = false;
+            playTimer.Elapsed += TimerOver;
+            playTimer.Start();
+        }
+        private static void TimerOver(Object source, ElapsedEventArgs e)
+        {
+            //TODO write something here nerd
+        }
+        void DeleteTimer()
+        {
+            if(playTimer.Disposed)
+                ColorConsole.WriteLine("[WARN] Attempted to dispose a timer that was already disposed!", ConsoleColor.Yellow);
+            else
+                playTimer.Dispose();
         }
     }
 }
