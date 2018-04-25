db3589bbe7279f197f8b0650775ba0f4a91cf2ad
DoggySazHi <william.le.huy@gmail.com>
2018-04-25 08:32:06 -0700

    please don't try to clone this

--- src/UNObot/Modules/Basecmds.cs
+++ src/UNObot/Modules/Basecmds.cs
@@ -39,5 +39,47 @@ namespace DiscordBot.Modules
         public Task Ugay3()
             => ReplyAsync(
                 $"<@{Context.User.Id}> no u\n");
+
+        [Command("help")]
+        public async Task Help()
+        {
+            await ReplyAsync("Help has been sent. Or, I think it has.");
+            await UserExtensions.SendMessageAsync(Context.Message.Author, "Commands: @UNOBot#4308 (Required) {Required in certain conditions} [Optional] " +
+                               "- Join\n" +
+                               "Join the queue.\n" +
+                               "- Leave" +
+                               "Leave the queue.\n" +
+                               "- Start\n" +
+                               "Start the game. Game only starts when 2 or more players are available.\n" +
+                               "- Draw\n" +
+                               "Get a card. This is randomized. Does not follow the 108 deck, but uses the probablity instead.\n" +
+                               "- Play (Color/Wild) (#/Reverse/Skip/+2/+4/Color) {Wild color change}\n" +
+                               "Play a card. You must have the card in your deck. Also, if you are placing a wildcard, type in the color as the next parameter.\n" +
+                               "- Card\n" +
+                               "See the last placed card.\n" +
+                               "- Deck See the cards you have currently.\n" +
+                               "- Uno\n" +
+                               "Don't forget to say this when you end up with one card left!\n" +
+                               "- Help\n" +
+                               "Get a help list. But you probably knew this.\n" +
+                              "- Seed (seed)\n" +
+                              "Possibly increases your chance of winning.\n" +
+                              "- Players\n" +
+                              "See who is playing and who's turn is it.\n" +
+                              "- Stats [player by mention]\n" +
+                              "See if you or somebody else is a pro or a noob at UNO. It's probably the former.\n" +
+                              "- Info\n" +
+                              "See the current version and other stuff about UNObot.");
+        }
+
+        [Command("credits")]
+        public async Task Credits()
+        {
+            await ReplyAsync("UNObot: Programmed by DoggySazHi\n" +
+                "Tested by Aragami and Fm\n" +
+                "Created for the UBOWS server\n\n" +
+                "Stickerz was here.\n" +
+                "Blame LocalDisk and Harvest for any bugs.");
+        }
     }
 }
