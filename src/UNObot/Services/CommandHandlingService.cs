51f6966544377ad521cc80faf7b370d3c2ed115f
DoggySazHi <william.le.huy@gmail.com>
2018-06-28 16:45:01 -0700

    Blame Molt for this fix. All it does is add the current card to .players.

--- src/UNObot/Services/CommandHandlingService.cs
+++ src/UNObot/Services/CommandHandlingService.cs
@@ -40,12 +40,12 @@ namespace DiscordBot.Services
             if (!(message.HasCharPrefix('.', ref argPos)) && !message.HasMentionPrefix(_discord.CurrentUser, ref argPos)) return;
 
             var context = new SocketCommandContext(_discord, message);
-
+            /*
             if(context.IsPrivate)
             {
                 await context.Channel.SendMessageAsync("I do not accept DM messages. Please use me in a guild/server.");
                 return;
-            }
+            }*/
             var result = await _commands.ExecuteAsync(context, argPos, _provider);
             if (result.Error.HasValue)
             {
